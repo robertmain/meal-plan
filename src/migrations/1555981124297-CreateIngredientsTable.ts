63eef53cabcbf045f469a69552f8dc3964f3d6c5
Robert Main <lenswipe@gmail.com>
2019-05-06 13:21:29 -0400

    Table references
    Store table and foreign key object references in migration class property. This makes it easier to tear the migration down later

--- src/migrations/1555981124297-CreateIngredientsTable.ts
+++ src/migrations/1555981124297-CreateIngredientsTable.ts
@@ -5,46 +5,47 @@ import {
 } from 'typeorm';
 
 export class CreateIngredientsTable1555981124297 implements MigrationInterface {
-  private tableName: string = 'ingredient';
+
+  private ingredientTable: Table = new Table({
+    name: 'ingredient',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'name',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'createdAt',
+        type: 'timestamp',
+        default: 'NOW()',
+        isNullable: false,
+      },
+      {
+        name: 'updatedAt',
+        type: 'timestamp',
+        default: 'NOW()',
+        isNullable: false,
+      },
+      {
+        name: 'deletedAt',
+        type: 'timestamp',
+        isNullable: true,
+      },
+    ],
+  })
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: this.tableName,
-      columns: [
-        {
-          name: 'id',
-          type: 'int',
-          isPrimary: true,
-          isGenerated: true,
-          generationStrategy: 'increment',
-        },
-        {
-          name: 'name',
-          type: 'varchar',
-          isNullable: false,
-        },
-        {
-          name: 'createdAt',
-          type: 'timestamp',
-          default: 'NOW()',
-          isNullable: false,
-        },
-        {
-          name: 'updatedAt',
-          type: 'timestamp',
-          default: 'NOW()',
-          isNullable: false,
-        },
-        {
-          name: 'deletedAt',
-          type: 'timestamp',
-          isNullable: true,
-        },
-      ],
-    }));
+    await queryRunner.createTable(this.ingredientTable);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable(this.tableName);
+    await queryRunner.dropTable(this.ingredientTable);
   }
 }
