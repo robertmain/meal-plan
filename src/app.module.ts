46e41b91b7c34ceb6930214f2f9e04ad9af3a97b
Robert Main <lenswipe@gmail.com>
2019-08-19 14:51:26 -0400

    basic webpack setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { IngredientModule } from './ingredient/ingredient.module';
 import { RecipeModule } from './recipe/recipe.module';
+import { Ingredient } from './ingredient/ingredient.entity';
+import { BaseEntity } from './base';
+import { Recipe } from './recipe/recipe.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'mealplan',
+      password: 'abc',
+      database: 'meal-plan',
+      entities: [
+        BaseEntity,
+        Ingredient,
+        Recipe,
+      ],
+      migrations: [join(__dirname, '/migration/**/*.ts')],
+    }),
     IngredientModule,
     RecipeModule,
   ],
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+
+const { IgnorePlugin, optimize } = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+/**
+ * NestJs uses a custom wrapper around require() that allows it to show a
+ * warning when some extra package needs to be installed. This causes problems
+ * with webpack, so we're blacklisting packages we're not using with the
+ * IgnorePlugin below.
+ *
+ * To de-blacklist a package, just remove it from this array.
+ */
+const nestBlacklist = [
+  '^class-validator$',
+  '^class-transformer$',
+  '^cache-manager$',
+  '^@nestjs/microservices$',
+  // packages below are required from microservices
+  '^amqp-connection-manager$',
+  '^amqplib$',
+  '^grpc$',
+  '^mqtt$',
+  '^nats$',
+  '^redis$',
+];
+
+const resolve = {
+  extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  plugins: [
+    new TSConfigPathsPlugin(),
+  ],
+};
+
+module.exports = ({ mode = 'development' }) => ({
+  mode,
+  entry: ['./src/main.ts'],
+  externals: [nodeExternals()],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  resolve,
+  context: path.resolve(__dirname),
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: true,
+      verbose: mode === 'development',
+    }),
+    new IgnorePlugin({
+      contextRegExp: /@nestjs/,
+      resourceRegExp: new RegExp(nestBlacklist.join('|')),
+    }),
+    new optimize.ModuleConcatenationPlugin(),
+  ],
+  node: {
+    __dirname: false,
+  },
+});
