ac1b1899d121c616e8e2be2cbc2e30d5a77556ec
Robert Main <lenswipe@gmail.com>
2019-11-29 20:37:41 -0500

    Only show docs in dev mode

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,22 @@ import { AppModule } from './app.module';
 import packageFile from '../package.json';
 
 const { name, description, version } = packageFile;
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, NODE_ENV } = process.env;
 
 (async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .setTitle((name[0].toUpperCase() + name.slice(1)).replace(/-/gi, ' '))
-    .setDescription(description)
-    .setVersion(version)
-    .addTag('ingredient')
-    .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  if (NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle((name[0].toUpperCase() + name.slice(1)).replace(/-/gi, ' '))
+      .setDescription(description)
+      .setVersion(version)
+      .addTag('ingredient')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
