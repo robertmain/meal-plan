6bb4eba0f300752f670ba552e49fe7a5c1d5321b
Robert Main <lenswipe@gmail.com>
2020-01-08 10:19:32 -0500

    Remove swagger decorators and let the framework genreate them automatically

--- src/ingredient/dto/createIngredient.dto.ts
+++ src/ingredient/dto/createIngredient.dto.ts
@@ -1,11 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined } from 'class-validator';
 
 export abstract class CreateIngredient {
   @IsDefined()
-  @ApiProperty({
-    description: 'The full name of the ingredient',
-    example: 'Quinoa',
-  })
   public name: string;
 }
--- src/recipe/dto/createRecipe.dto.ts
+++ src/recipe/dto/createRecipe.dto.ts
@@ -1,25 +1,13 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsDefined, Allow } from 'class-validator';
 import { Ingredient } from '../../ingredient/ingredient.entity';
 
 export abstract class CreateRecipe {
   @IsDefined()
-  @ApiProperty({
-    description: 'The name of the recipe',
-    example: 'Hearty beef stew',
-  })
   public name: string;
 
   @Allow()
-  @ApiPropertyOptional({
-    description: 'An optional description of the recipe to create',
-    example: 'A thick beef stew with a rich mushroom gravy',
-  })
   public description?: string = '';
 
   @Allow()
-  @ApiPropertyOptional({
-    description: 'An array of ingredients to assign to this recipe',
-  })
   public ingredients?: Ingredient[] = [];
 }
