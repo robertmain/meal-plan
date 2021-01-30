b7673efebd93c9ddfd06b40acb5433d6f11318b6
Robert Main <lenswipe@gmail.com>
2021-01-30 14:48:53 -0500

    Fix eslintrc indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,32 @@
 module.exports = {
-  'parser':  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  'extends': [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'airbnb-base'
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'airbnb-base',
   ],
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true,
-    'jest': true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  'rules': {
+  rules: {
     '@typescript-eslint/indent': ['error', 2],
-    'indent': ['error', 2],
-    'arrow-parens': [
-      'warn',
-      'always'
-    ],
+    indent: ['error', 2],
+    'arrow-parens': ['warn', 'always'],
     'comma-dangle': [
       'error',
       {
-        'arrays': 'always-multiline',
-        'objects': 'always-multiline',
-        'imports': 'always-multiline',
-        'exports': 'always-multiline',
-        'functions': 'never'
-      }
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
     ],
     'consistent-return': 'off',
     'func-names': ['warn', 'as-needed'],
@@ -37,60 +34,62 @@ module.exports = {
     'import/no-extraneous-dependencies': [
       'error',
       {
-        'devDependencies': true
-      }
+        devDependencies: true,
+      },
     ],
     'import/prefer-default-export': 'off',
     'max-len': [
       'error',
       {
-        'code': 80,
-        'ignoreComments': true,
-        'ignoreStrings': true,
-        'ignoreTemplateLiterals': true,
-        'ignoreUrls': true,
-        'ignoreRegExpLiterals': true,
-        'tabWidth': 2
-      }
+        code: 80,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+        ignoreRegExpLiterals: true,
+        tabWidth: 2,
+      },
     ],
     'max-params': [
       'warn',
       {
-        'max': 3
-      }
+        max: 3,
+      },
     ],
     'no-plusplus': [
-      'warn', {
-        'allowForLoopAfterthoughts': true
-      }
+      'warn',
+      {
+        allowForLoopAfterthoughts: true,
+      },
     ],
     'no-underscore-dangle': 'off',
     'prefer-arrow-callback': 0,
     'prefer-template': 'off',
-    'prefer-destructuring': ['error',
-    {
-      'VariableDeclarator': {
-        'array': false,
-        'object': true
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: true,
+        },
       },
-      'AssignmentExpression': {
-        'array': false,
-        'object': true
-      }
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    radix: 'off',
+    'class-methods-use-this': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.ts'],
+      },
     },
-    {
-      'enforceForRenamedProperties': false
-    }
-  ],
-  'radix': 'off',
-  'class-methods-use-this': 'off'
-},
-'settings': {
-  'import/resolver': {
-    'node': {
-      'paths': ['src'],
-      'extensions': ['.ts']
-    }
-  }
-}
+  },
 };
