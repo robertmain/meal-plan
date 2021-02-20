a9112720cfe3e8296575fe158326c20674086f66
Robert Main <lenswipe@gmail.com>
2021-02-19 21:16:11 -0500

    Setup base front-end app to begin development

--- src/client/app/router/index.ts
+++ src/client/app/router/index.ts
@@ -7,15 +7,7 @@ const routes: RouteConfig[] = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('@/views/Home.vue'),
   },
 ];
 
