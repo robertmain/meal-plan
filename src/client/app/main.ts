bb69f43232f1e9705cb3cef429a75030ad6975c6
Robert Main <lenswipe@gmail.com>
2021-02-23 00:48:18 -0500

    Import individual element-ui components where they are used
    This avoids having a gigantic array of imported code that may not be
    even being used

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,37 +1,10 @@
 import Vue from 'vue';
-import {
-  Button,
-  Container,
-  Divider,
-  Header,
-  Icon,
-  Input,
-  Main,
-  Menu,
-  MenuItem,
-  Submenu,
-} from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'reset.css';
 import 'element-ui/lib/theme-chalk/reset.css';
 import 'element-ui/lib/theme-chalk/index.css';
-
-[
-  Button,
-  Container,
-  Divider,
-  Header,
-  Icon,
-  Input,
-  Main,
-  Menu,
-  MenuItem,
-  Submenu,
-].forEach((component) => {
-  Vue.component(component.name, component);
-});
 
 Vue.config.productionTip = false;
 new Vue({
