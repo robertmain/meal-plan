d44b2580af6af67732beb3fda2e93af8e1d6fc13
Robert Main <lenswipe@gmail.com>
2021-03-18 21:44:14 -0400

    home page

--- src/client/app/views/Home.vue
+++ src/client/app/views/Home.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
-    <!-- Home Sweet Home -->
+    <page-section background-color="var(--app-primary)" title="Sign In">
+      <card v-for="index2 in Array(1).fill('')" :key="index2">
+        <h2>Hello World</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eaque,
+          sapiente a odio blanditiis eos unde minima adipisci consectetur,
+          exercitationem eligendi culpa soluta perferendis quaerat nisi.
+          Eos quidem rerum soluta! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit.
+        </p>
+      </card>
+    </page-section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import PageSection from '@/components/PageSection.vue';
+import Card from '@/components/Card.vue';
 
 @Component({
-  components: { },
+  components: {
+    PageSection,
+    Card,
+  },
 })
 export default class Home extends Vue { }
 </script>
+
+<style lang="scss" scoped>
+/deep/ section{
+  display: grid;
+}
+</style>
