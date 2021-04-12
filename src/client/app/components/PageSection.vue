c78cc83e31f1385e0a0818a0306479a57037ab7f
Robert Main <lenswipe@gmail.com>
2021-04-11 22:26:10 -0400

    Implement page sections

---
+++ src/client/app/components/PageSection.vue
@@ -0,0 +1,34 @@
+<template functional>
+  <section :style="{ backgroundColor: props.backgroundColor }">
+    <h2 v-if="props.title">
+      {{ props.title }}
+    </h2>
+    <slot />
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: { },
+})
+export default class Home extends Vue {
+  @Prop({ required: false })
+  public title: string;
+
+  @Prop({ required: false })
+  public backgroundColor: string;
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+  padding: var(--spacing-md);
+  line-height: 2;
+  h2{
+    margin-bottom: var(--spacing-md);
+    color: white;
+  }
+}
+</style>
