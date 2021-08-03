95f0a8f44cccf8217f6084e3ced9f7b492a5a5c1
Robert Main <lenswipe@gmail.com>
2021-08-03 00:19:59 -0400

    Action to commit recipes from the API to the store

--- src/client/app/store/modules/recipe/actions.spec.ts
+++ src/client/app/store/modules/recipe/actions.spec.ts
@@ -1,5 +1,83 @@
+import { RootState } from '@/store/state';
+import { MutationTree, Store } from 'vuex';
+import * as RecipeApi from '../../../api/Recipe';
+import { createFakeStore } from '../../../../jest-helpers';
+import { actions, ACTIONS as ACTION_TYPES } from './actions';
+import { State } from './state';
+import { MUTATIONS, mutations } from './mutations';
+
 describe('Actions', () => {
-  it('', () => {
-    // ...
+  let fakeStore: Store<RootState>;
+  let fakeMutations: MutationTree<State>;
+  beforeEach(() => {
+    fakeMutations = Object.keys(mutations)
+      .reduce((acc, name) => {
+        acc[name] = jest.fn();
+        return acc;
+      }, {} as MutationTree<State>);
+
+    fakeStore = createFakeStore({
+      mutations: fakeMutations,
+      actions,
+    });
+  });
+  describe('getRecipes', () => {
+    it('commits a mutation to indicate the start of an AJAX process', () => {
+      fakeStore.dispatch(ACTION_TYPES.GET_RECIPES);
+
+      expect(fakeMutations[MUTATIONS.GET_RECIPES_START])
+        .toHaveBeenCalledTimes(1);
+    });
+    it('commits a mutation to indicate the success of the AJAX process', async () => {
+      jest.spyOn(RecipeApi, 'getRecipes')
+        .mockImplementationOnce(() => Promise.resolve({
+          config: {},
+          data: [],
+          headers: [],
+          status: 200,
+          statusText: 'OK',
+        }));
+
+      await fakeStore.dispatch(ACTION_TYPES.GET_RECIPES);
+
+      expect(fakeMutations[MUTATIONS.GET_RECIPES_SUCCEEDED])
+        .toHaveBeenCalledTimes(1);
+    });
+    it('commits a mutation to indicate the failure of the AJAX process', async () => {
+      const fakeError = new Error('This broke');
+      jest.spyOn(RecipeApi, 'getRecipes')
+        .mockImplementationOnce(() => Promise.reject(fakeError));
+
+      await fakeStore.dispatch(ACTION_TYPES.GET_RECIPES);
+
+      expect(fakeMutations[MUTATIONS.GET_RECIPES_FAILED])
+        .toHaveBeenCalledTimes(1);
+      expect(fakeMutations[MUTATIONS.GET_RECIPES_FAILED])
+        .toHaveBeenCalledWith({}, [fakeError]);
+    });
+    it('commits the recipes returned to the store', async () => {
+      const recipes = [
+        {
+          id: 'e585b67c-cb09-47e6-bb39-a8d5494fc986',
+          name: 'My Recipe',
+          description: '',
+          ingredients: [],
+        },
+      ];
+
+      jest.spyOn(RecipeApi, 'getRecipes')
+        .mockImplementationOnce(() => Promise.resolve({
+          config: {},
+          headers: [],
+          status: 200,
+          statusText: 'OK',
+          data: recipes,
+        }));
+
+      await fakeStore.dispatch(ACTION_TYPES.GET_RECIPES);
+
+      expect(fakeMutations[MUTATIONS.GET_RECIPES_SUCCEEDED])
+        .toHaveBeenCalledWith({}, recipes);
+    });
   });
 });
--- src/client/app/store/modules/recipe/actions.ts
+++ src/client/app/store/modules/recipe/actions.ts
@@ -1,7 +1,23 @@
+import { getRecipes } from '@/api/Recipe';
 import { RootState } from '@/store/state';
 import { ActionTree } from 'vuex';
+import { MUTATIONS } from './mutations';
 import { State } from './state';
 
+export enum ACTIONS {
+  GET_RECIPES = 'GET_RECIPES',
+}
+
 export const actions: ActionTree<State, RootState> = {
-  // ...
+  [ACTIONS.GET_RECIPES]: async ({ commit }) => {
+    commit(MUTATIONS.GET_RECIPES_START);
+    try {
+      const {
+        data,
+      } = await getRecipes();
+      commit(MUTATIONS.GET_RECIPES_SUCCEEDED, data);
+    } catch (e) {
+      commit(MUTATIONS.GET_RECIPES_FAILED, [e]);
+    }
+  },
 };
